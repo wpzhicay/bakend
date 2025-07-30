6e25f8794262266925e3115efadad6f9115b592a
wilson <pwzhicay@sudamericano.edu.ec>
2025-07-29 23:48:19 -0500

    Corrigiendo ruta raíz

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -11,5 +13,7 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     UsersModule,
   ],
+  controllers: [AppController], // 👈 Agrega esto
+  providers: [AppService],      // 👈 Y esto también
 })
 export class AppModule {}
